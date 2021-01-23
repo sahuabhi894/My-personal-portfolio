81b2cd16297d5dc51e2a981495a892021907b9f6
_ <2019kuec2026@iiitkota.ac.in>
2021-01-23 11:17:05 +0530

    hello

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Job
+
+def homepage(request):
+    home = Job.objects
+    return render(request, 'home/homepage.html', {'home':home})
+    
