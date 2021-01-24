7fef7e6b645b819cc624b09ac76f9243bf0d00ec
_ <2019kuec2026@iiitkota.ac.in>
2021-01-24 09:48:26 +0530

    up

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 
 from django.urls import path
-import .views
+from . import views
+
 
 
 
 urlpatterns = [
     
     path('',views.allblogs,name='allblogs'),
+    path('<int:blog_id>/',views.detail , name='detail'),
 ]
